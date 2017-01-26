44136cc5f86b82f1d61515bbee63458de827ff8b
barryosullivan <113497392@umail.ucc.ie>
2017-01-26 00:05:48 +0000

    Update
    Update

--- interface/temp.py
+++ interface/temp.py
@@ -1,25 +1,22 @@
-"""
-***User Interface Views***
-"""
-from django.shortcuts import render, HttpResponse
+import os
+import time
+import datetime
 from urllib.request import urlopen
 import json
 
+i = datetime.datetime.now()
 
-
-#Index Page for the website
-#Sends for a JSON file and parses the data into a Database
-def index( request ):
+def read_temp():
     weather_station = '/IE/Cork'
     heating_system_status = False
     key = 'a7e9c74ceafd65c5'
-
-    #HTTP Request to the Wunderground website returns JSON current conditions
+	
+	#HTTP Request to the Wunderground website returns JSON current conditions
     conditions = urlopen('http://api.wunderground.com/api/{}/geolookup/conditions/q{}.json'.format(key, weather_station))
     conditions_string = conditions.read().decode('utf-8')
     parsed_conditions = json.loads(conditions_string)
-
-    #HTTP Request to the Wunderground website returns JSON forecast   
+	
+	 #HTTP Request to the Wunderground website returns JSON forecast   
     forecast = urlopen('http://api.wunderground.com/api/{}/forecast/q{}.json'.format(key, weather_station))
     forecast_string = forecast.read().decode('utf-8')
     parsed_forecast = json.loads(forecast_string)
@@ -34,15 +31,18 @@ def index( request ):
 
     conditions.close()
     forecast.close()
+	
+    while True:    
+        print("recording data into database(period = 5s.)....press ctrl+C to stop!") 	
+        year = str(i.year)
+        month = str(i.month)
+        day = str(i.day)
+        date = day + "-" + month + "-" + year
+        hour = str(i.hour)
+        minute = str(i.minute)
+        second = str(i.second)
+        timestr = hour + ":" + minute + ":" + second
+        time.sleep(10)
 
-    context = {
-    'weather_location': location,
-    'weather_tempc': temp_c,
-    'windkph': windkph,
-    'humidity': relative_humidity,
-    'status' : heating_system_status,
-    'chance_rain' : chance_rain,
-    'high' : high,
-    'low' : low,
-    }
-    return render( request, 'interface/index.html', context )
+if __name__ == "__main__":
+    read_temp()
